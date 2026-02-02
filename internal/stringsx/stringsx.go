8c64e06fc755237076cd16ac62485c506278e83a
Victor Smol <smolyakov.92@bk.ru>
2026-02-03 00:34:13 +0500

    Добавил stringsx.go

---
+++ internal/stringsx/stringsx.go
@@ -0,0 +1,29 @@
+package stringsx
+
+/*
+Реализовать пакет stringsx:
+- нормализация строк
+- безопасный split/join
+- парсинг “key=value;key2=value2”
+*/
+
+// Normalize нормализует строку.
+func Normalize(s string) string {
+
+	return ""
+}
+
+// Split разбивает строку на подстроки по разделителю.
+func Split(s string, sep string) []string {
+	return []string{}
+}
+
+// Join объединяет подстроки в строку с разделителем.
+func Join(s []string, sep string) string {
+	return ""
+}
+
+// ParseKV парсит строку в map.
+func ParseKV(s string) map[string]string {
+	return map[string]string{}
+}
